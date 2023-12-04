cae2b78fdbda035a841b2c996f9cc4a77f4f2810
MSI\MSIPC <hendridrusmana@gmail.com>
2023-12-04 20:18:34 +0700

    sudah diubah

---
+++ Point1.java
@@ -0,0 +1,15 @@
+package DataAbstraction;
+
+public interface Point1 {
+    double getX();
+
+    double getY();
+
+    void setCartesian(double x, double y);
+
+    double getR();
+
+    double getTheta();
+
+    void setPolar(double r, double theta);
+}
