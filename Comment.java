1be1f8c37e1c79cb59d3129b16a34bd198359e7e
MSI\MSIPC <hendridrusmana@gmail.com>
2023-12-04 20:21:39 +0700

    sudah diubah latihan 4

---
+++ Comment.java
@@ -0,0 +1,15 @@
+public class Comment {
+    // Copyright (C) 2011 by Osoco. All rights reserved.
+    // Released under the terms of the GNU General Public License
+    // version 2 or later.
+    // Returns an instance of the Responder being tested.
+    protected abstract Responder responderInstance();
+    // renaming the function: responderBeingTested
+    
+
+    // format matched kk:mm:ss EEE, MMM dd, yyyy
+Pattern timeMatcher = Pattern.compile("\\d*:\\d*:\\d* \\w*, \\w*
+\\d*,\\d*");
+assertTrue(a.compareTo(b) == -1); // a < b
+assertTrue(b.compareTo(a) == 1); // b > a
+}
