847cae8d15544a0216c4519908b092b6cf242f5a
MSI\MSIPC <hendridrusmana@gmail.com>
2023-12-04 20:11:32 +0700

    sudah diubah

---
+++ Customer.java
@@ -0,0 +1,11 @@
+package UsePronounceableNames;
+
+import java.util.Date;
+
+public class Customer {
+
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+    /* ... */
+}
