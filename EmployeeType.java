2d73d69bca86d634b169998c1a961ebc0a015a36
MSI\MSIPC <hendridrusmana@gmail.com>
2023-12-04 20:14:35 +0700

    sudah diubah

---
+++ EmployeeType.java
@@ -0,0 +1,17 @@
+package SwitchStatements;
+
+public class EmployeeType {
+    abstract int payAmount(Employee emp);
+}
+
+class Salesman {
+    int payAmount(Employee emp) {
+        return emp.getMonthlySalary() + emp.getCommission();
+    }
+}
+
+class Manager {
+    int payAmount(Employee emp) {
+        return emp.getMonthlySalary() + emp.getBonus();
+    }
+}
